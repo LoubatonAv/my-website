b2d2280708b398a1793eb50356088fb32a84c738
LoubatonAv <aegtar88@gmail.com>
2022-12-15 19:25:32 +0200

    fix link

--- src/cmps2/Projects.jsx
+++ src/cmps2/Projects.jsx
@@ -12,7 +12,7 @@ import { TypeAnimation } from 'react-type-animation';
 const Projects = () => {
   return (
     //link to projects
-    <div className='' id='projects'>
+    <div id='projects'>
       <h1 className='pt-32 text-5xl pb-10 text-center font-gloria italic'>
         Some of my projects <span className='text-xs '>(so far...)</span>
       </h1>
