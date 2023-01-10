e708524f7fa6ba562d4dd5c3921e195f370329e4
LoubatonAv <aegtar88@gmail.com>
2023-01-10 02:18:19 +0200

    fix addresses

--- src/cmps2/Projects.jsx
+++ src/cmps2/Projects.jsx
@@ -55,6 +55,7 @@ const Projects = () => {
                     class='rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 hover:bg-white hover:text-black'>
                     GitHUB
                   </a>
+                  {/* address of website */}
                   <a
                     href='https://hosty-backend-muhm.onrender.com/'
                     target='_blank'
